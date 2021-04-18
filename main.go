652eb4f79d4acf22e253674298a2b8fb6c28eed0
openkubeio <pramode2009@gmail.com>
2021-04-18 14:08:59 +0530

    ok

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "io/ioutil"
+    H "./handlers" 
+    P "./handlers/pods"
+)
+
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+
+    fmt.Fprintf(w, r.URL.Path )
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+    if r.URL.Path != "/hello" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported.", http.StatusNotFound)
+        return
+    }
+
+    fmt.Fprintf(w, "Hello!")
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+
+    resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+    if err != nil {
+       log.Fatalln(err)
+    }
+
+    // Read the response body on the line below.
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+       log.Fatalln(err)
+    }
+    
+    // Convert the body to type string
+    sb := string(body)
+    fmt.Fprintf(w, sb)
+}
+
+
+
+func main() {
+
+    http.HandleFunc("/",     defaultHandler)
+    http.HandleFunc("/hello",helloHandler)
+    http.HandleFunc("/test", testHandler)
+    
+    H.PreBuild()
+    H.PostBuild()
+    P.GetPod()
+    
+    log.Printf("Starting server at port 8080\n")
+
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
